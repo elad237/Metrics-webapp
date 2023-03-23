594c29c298aaf61eff275930b8e6e3482504d8d1
elad237 <tumbu.elad@gmail.com>
2023-03-23 14:23:10 +0100

    Updated the src folder in order to add the styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
-import Home from './pages/Home';
+import Stocks from './pages/Stocks';
+import Stock from './pages/Stock';
 import Show from './pages/Show';
 import About from './pages/About';
 import NoMatch from './pages/NoMatch';
@@ -10,8 +11,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="show" element={<Show />} />
+        <Route index element={<Stocks />} />
+        <Route path="stocks" element={<Stocks />} />
+        <Route path="stock" element={<Stock />}>
+          <Route path=":slug" element={<Show />} />
+        </Route>
         <Route path="about" element={<About />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
--- src/components/StocksContainer.jsx
+++ src/components/StocksContainer.jsx
@@ -1,27 +1,69 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { fetchStocks } from '../redux/stocks/stocksSlice';
-import Stock from './Stock';
+import './StocksContainer.scss';
 
 const StocksContainer = () => {
   const dispatch = useDispatch();
   const ifSucceed = useSelector((store) => store.stocks.ifSucceed);
   const stocksArray = useSelector((store) => store.stocks.stocksArray);
+  const sum = stocksArray.reduce((n, { marketCap }) => n + marketCap, 0);
 
   useEffect(() => {
-    dispatch(fetchStocks());
-  }, [dispatch, ifSucceed]);
+    if (stocksArray.length === 0) {
+      dispatch(fetchStocks());
+    }
+  }, [dispatch, ifSucceed, stocksArray]);
 
   return (
-    <>
-      <h3>LIST</h3>
-      {stocksArray.map((stock) => (
-        <Stock
-          key={stock.symbol}
-          companyName={stock.companyName}
-        />
-      ))}
-    </>
+    <main>
+      <section className="total">
+        <h1>
+          STOCKS TOTAL MARKET CAP
+          <br />
+          {sum.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+          })}
+        </h1>
+      </section>
+      <section className="wrap">
+        {stocksArray.map((stock, idx) => (idx % 2 === 0 ? (
+          <span className="stock even" key={stock.symbol}>
+            <NavLink to={`stock/${stock.symbol}`}>
+              {stock.companyName}
+              <br />
+              Market Cap
+              <br />
+              {stock.marketCap.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </NavLink>
+          </span>
+        ) : (
+          <span className="stock odd" key={stock.symbol}>
+            <NavLink to={`stock/${stock.symbol}`}>
+              {stock.companyName}
+              <br />
+              Market Cap
+              <br />
+              {stock.marketCap.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </NavLink>
+          </span>
+        )))}
+      </section>
+    </main>
   );
 };
 
